c8df4d13eec424e2133e4f790f943f38b53d47cb
leofinder <imrhyde@yandex.ru>
2023-09-16 18:25:06 +0500

    Балансир [#505083]

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,18 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        int index = 0;
+        while (source.hasNext()) {
+            if (index == nodes.size()) {
+                index = 0;
+            }
+            nodes.get(index).add(source.next());
+            index++;
+        }
+    }
+}
