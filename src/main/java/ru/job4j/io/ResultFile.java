97ee11c1788e1bfe1d0bd05563f6a68781e071ff
leofinder <imrhyde@yandex.ru>
2023-11-18 14:21:22 +0500

    BufferedOutputStream

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,13 +1,14 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
-            out.write("Hello world!".getBytes());
-            out.write(System.lineSeparator().getBytes());
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream("data/dataresult.txt")))) {
+            out.println("Hello world!");
         } catch (IOException e) {
             e.printStackTrace();
         }
