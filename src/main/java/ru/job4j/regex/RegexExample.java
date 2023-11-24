d733c72f54fd564d3d0e149bab0508906ac7c8a3
leofinder <imrhyde@yandex.ru>
2023-11-24 21:42:01 +0500

    Регулярные выражения

---
+++ src/main/java/ru/job4j/regex/RegexExample.java
@@ -0,0 +1,15 @@
+package ru.job4j.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexExample {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("\\b.+@.+\\..+\\b");
+        String text = "test.test@gmail.com example65@mail_boxru 123_45@example-mailbox.com";
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println("Найдено совпадение: " + matcher.group());
+        }
+    }
+}
