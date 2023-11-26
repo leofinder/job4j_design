81785a9de69afb6a4638335944ea44440d993a17
leofinder <imrhyde@yandex.ru>
2023-11-26 14:18:15 +0500

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,20 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Leo Hyde";
+        boolean isHuman = true;
+        byte age = 37;
+        float temperature = 36.6F;
+        double accountBalance = 673409.67;
+
+        short year = 2027;
+        int populationOfCity = 498000;
+        long distanceToMoon = 384400000L;
+        char dangerLevel = 'B';
+
+        LOG.debug("User info name: {}, human: {}, age: {}, temperature: {}, balance: {}",
+                name, isHuman, age, temperature, accountBalance);
+        LOG.debug("Current year: {}, population of city: {}, danger level: {}, distance to moon: {}",
+                year, populationOfCity, dangerLevel, distanceToMoon);
     }
 }
